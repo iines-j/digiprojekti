9f07f3830c482d78f06edf9905d666826ff79f56
IJ <ij@gmail.com>
2025-12-07 20:22:14 +0200

    edited journal

--- scenes/journal/journal.gd
+++ scenes/journal/journal.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("RESET")
+	# $AnimationPlayer.play("RESET")
 	pause()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +18,10 @@ func resume():
 	
 func pause():
 	get_tree().paused = true
-	$AnimationPlayer.play("blur")
+	$AnimationPlayer.play("blur/blur 2")
 	
 	# on back btn set paused to false!
+
+func _on_back_pressed() -> void:
+	global.main.change_gui_scene("res://scenes/pause_menu/PauseMenu.tscn")
+	get_tree().paused = false
